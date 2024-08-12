1a1be405f79cdb98420a706706e46d30f96773d9
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-12 18:09:41 +0200

    basic working game

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,6 @@
 const body = document.querySelector("body");
+const input = document.querySelectorAll(".guess");
+console.log(input);
 const colorDiv = document.querySelector(".color");
 const hex = ['1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F'];
 
@@ -16,18 +18,28 @@ let answer = randomHex();
 colorDiv.style.background = answer;
 let round = 0;
 let letter = 0; 
+let playerAnswer = "#";
 body.addEventListener("keypress", (event) => {
-  const input = document.querySelectorAll(".guess");
   if (hex.includes(event.key.toUpperCase())) {
     if (letter < 6) {
       input[round].children[letter+1].textContent = event.key.toUpperCase();
+      playerAnswer += event.key.toUpperCase();
       letter += 1;
     }
-  }
-  if (event.key == "Enter") {
-    console.log("enter")
-  }
-})
+  } 
+  console.log(round)
+ if (event.key == "Enter" && round == 5 && letter == 6) {
+    console.log("You lost!");
+    console.log("The answer was"+answer);
+  } else if (event.key == "Enter" && letter == 6) {
+    if (answer == playerAnswer) {
+      console.log("You won!");
+    } else {
+      console.log("next round!");
+      round += 1;
+      letter = 0;
+    } 
+  }})
 }
 
 newGame();
